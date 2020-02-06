3c334d28bd0406e65709b2d87fd82b40052835c2
FRYoussef <f.r.youssef@hotmail.com>
2020-02-06 14:48:08 +0100

    Added project source

---
+++ practica1/ProyectosIG_x64_VS2019_FG/IG1App/Entity.cpp
@@ -0,0 +1,62 @@
+#include "Entity.h"
+
+#include <gtc/matrix_transform.hpp>  
+#include <gtc/type_ptr.hpp>
+
+using namespace glm;
+
+//-------------------------------------------------------------------------
+
+void Abs_Entity::upload(dmat4 const& modelViewMat) const 
+{ 
+	glMatrixMode(GL_MODELVIEW);
+	glLoadMatrixd(value_ptr(modelViewMat));  // transfers modelView matrix to the GPU
+}
+//-------------------------------------------------------------------------
+//-------------------------------------------------------------------------
+
+EjesRGB::EjesRGB(GLdouble l): Abs_Entity()
+{
+  mMesh = Mesh::createRGBAxes(l);
+}
+//-------------------------------------------------------------------------
+
+EjesRGB::~EjesRGB() 
+{ 
+	delete mMesh; mMesh = nullptr; 
+};
+//-------------------------------------------------------------------------
+
+void EjesRGB::render(dmat4 const& modelViewMat) const 
+{
+	if (mMesh != nullptr) {
+		dmat4 aMat = modelViewMat * mModelMat;  // glm matrix multiplication
+		upload(aMat);
+		glLineWidth(2);
+		mMesh->render();
+		glLineWidth(1);
+	}
+}
+//-------------------------------------------------------------------------
+
+Poligono::Poligono(GLuint numL, GLdouble rd) {
+	this->numL = numL;
+	this->rd = rd;
+	mMesh = Mesh::generaPoligono(numL, rd);
+}
+
+void Poligono::render(dmat4 const& modelViewMat) const
+{
+	if (mMesh != nullptr) {
+		dmat4 aMat = modelViewMat * mModelMat;  // glm matrix multiplication
+		upload(aMat);
+		glLineWidth(2);
+		glColor3d(mColor.r, mColor.g, mColor.b);
+		mMesh->render();
+
+		//Resetear attrs
+		glLineWidth(1);
+		glColor3d(1, 1, 1);
+	}
+}
+ 
---
+++ practica1/ProyectosIG_x64_VS2019_FG/IG1App/Mesh.cpp
@@ -0,0 +1,90 @@
+#include "Mesh.h"
+#include "CheckML.h"
+#include <fstream>
+using namespace std;
+using namespace glm;
+
+//-------------------------------------------------------------------------
+
+void Mesh::draw() const 
+{
+  glDrawArrays(mPrimitive, 0, size());   // primitive graphic, first index and number of elements to be rendered
+}
+//-------------------------------------------------------------------------
+
+void Mesh::render() const 
+{
+  if (vVertices.size() > 0) {  // transfer data
+    // transfer the coordinates of the vertices
+    glEnableClientState(GL_VERTEX_ARRAY);
+    glVertexPointer(3, GL_DOUBLE, 0, vVertices.data());  // number of coordinates per vertex, type of each coordinate, stride, pointer 
+    if (vColors.size() > 0) { // transfer colors
+      glEnableClientState(GL_COLOR_ARRAY);
+      glColorPointer(4, GL_DOUBLE, 0, vColors.data());  // components number (rgba=4), type of each component, stride, pointer  
+    }
+
+	draw();
+
+    glDisableClientState(GL_COLOR_ARRAY);
+	glDisableClientState(GL_VERTEX_ARRAY);
+  }
+}
+//-------------------------------------------------------------------------
+
+Mesh * Mesh::createRGBAxes(GLdouble l)
+{
+  Mesh* mesh = new Mesh();
+
+  mesh->mPrimitive = GL_LINES;
+
+  mesh->mNumVertices = 6;
+  mesh->vVertices.reserve(mesh->mNumVertices);
+
+  // X axis vertices
+  mesh->vVertices.emplace_back(0.0, 0.0, 0.0);
+  mesh->vVertices.emplace_back(l, 0.0, 0.0);
+  // Y axis vertices
+  mesh->vVertices.emplace_back(0, 0.0, 0.0);
+  mesh->vVertices.emplace_back(0.0, l, 0.0);
+  // Z axis vertices
+  mesh->vVertices.emplace_back(0.0, 0.0, 0.0);
+  mesh->vVertices.emplace_back(0.0, 0.0, l);
+
+  mesh->vColors.reserve(mesh->mNumVertices);
+  // X axis color: red  (Alpha = 1 : fully opaque)
+  mesh->vColors.emplace_back(1.0, 0.0, 0.0, 1.0);
+  mesh->vColors.emplace_back(1.0, 0.0, 0.0, 1.0);
+  // Y axis color: green
+  mesh->vColors.emplace_back(0.0, 1.0, 0.0, 1.0);
+  mesh->vColors.emplace_back(0.0, 1.0, 0.0, 1.0);
+  // Z axis color: blue
+  mesh->vColors.emplace_back(0.0, 0.0, 1.0, 1.0);
+  mesh->vColors.emplace_back(0.0, 0.0, 1.0, 1.0);
+ 
+  return mesh;
+}
+//-------------------------------------------------------------------------
+
+Mesh* Mesh::generaPoligono(GLuint numL, GLdouble rd) {
+    Mesh* mesh = new Mesh();
+    mesh->mPrimitive = GL_LINE_LOOP;
+    mesh->mNumVertices = numL;
+    mesh->vVertices.reserve(mesh->mNumVertices);
+    double vertex_X = 0;
+    double vertex_Y = 0;
+    double ang = 90.0;
+    double incr = 360.0 / numL;
+
+    for (int i = 0; i < numL; i++) {
+        vertex_X = rd * cos(radians(ang));
+        vertex_Y = rd * sin(radians(ang));
+         
+        mesh->vVertices.emplace_back(vertex_X, vertex_Y, 0.0);
+
+        ang += incr;
+    }
+
+
+    return mesh;
+}
+
